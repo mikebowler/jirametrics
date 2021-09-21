d405e9940527a665f30641b04561e8fe3b6ec273
Mike Bowler <mbowler@gargoylesoftware.com>
2021-09-21 13:20:00 -0700

    Added support for methods in Issue to have a config object be passed in. This is needed for the new status category stuff that is coming next

--- lib/config_base.rb
+++ lib/config_base.rb
@@ -1,94 +1,118 @@
 require 'csv'
 
+class ExportColumns < BasicObject
+    attr_reader :columns
+
+    def initialize config
+        @columns = []
+        @config = config
+    end
+
+    def date label, block
+        @columns << [:date, label, block]
+    end
+
+    def string label, block
+        @columns << [:string, label, block]
+    end
+
+    # Why is this here? Because I keep forgetting that puts() will be caught by method_missing and
+    # that makes me spin through a debug cycle. So I make it do the expected thing.
+    def puts *args
+        $stdout.puts *args
+    end
+
+    def method_missing method_name, *args, &block
+        # Have to reference config outside the lambda so that it's accessible inside.
+        # When the lambda is executed for real, it will be running inside the context of an Issue
+        # object and at that point @config won't be referencing a variable from the right object.
+        config = @config
+
+        -> (issue) do
+            parameters = issue.method(method_name.to_sym).parameters
+            # Is the first parameter called config?
+            if parameters.empty? == false && parameters[0][1] == :config
+                new_args = [config] + args
+                issue.__send__ method_name, *new_args
+            else
+                issue.__send__ method_name, *args 
+            end
+        end # *([config] + args) }
+    end
+end
+
 # The goal was to make both the configuration itself and the issue/loader
 # objects easy to read so the tricky (specifically meta programming) parts 
 # are all in here. Be cautious when changing this file.
 class ConfigBase
-	class ExportColumns < BasicObject
-		attr_reader :columns
-		def initialize = @columns = []
+    attr_reader :issues, :file_prefix, :jql, :project_key
+    @@target_path = 'target/'
+    @@configs = []
 
-		def date label, block
-			@columns << [:date, label, block]
-		end
+    def self.export file_prefix:, project: nil, filter: nil, jql: nil, &block
+        instance = ConfigBase.new(
+            file_prefix: file_prefix, project: project, filter: filter, jql: jql, export_config_block: block
+        )
+        @@configs << instance
+    end
 
-		def string label, block
-			@columns << [:string, label, block]
-		end
+    # Does nothing. An easy way to comment out a project
+    def self.xexport *args
+    end
 
-		def method_missing method_name, *args, &block
-			-> (issue) { issue.__send__ method_name, *args }
-		end
-	end
+    def self.target_path(path) = @@target_path = path
+    def self.instances = @@configs
 
-	attr_reader :issues, :file_prefix, :jql, :project_key
-	@@target_path = 'target/'
-	@@configs = []
+    def initialize file_prefix:, project: nil, filter: nil, jql: nil, export_config_block: nil
+        @file_prefix = file_prefix
+        @csv_filename = "#{@@target_path}/#{file_prefix}.csv"
+        @export_config_block = export_config_block
+        @jql = make_jql project: project, filter: filter, jql: jql
+        @project_key = project
+    end
 
-	def self.export file_prefix:, project: nil, filter: nil, jql: nil, &block
-		instance = ConfigBase.new(
-			file_prefix: file_prefix, project: project, filter: filter, jql: jql, export_config_block: block
-		)
-		@@configs << instance
-	end
+    def make_jql project:, filter:, jql:
+        return jql unless jql.nil?
+        return "project=#{project.inspect}" unless project.nil?
+        return "filter=#{filter.inspect}" unless filter.nil?
+        raise "Everything was nil"
+    end
 
-	# Does nothing. An easy way to comment out a project
-	def self.xexport *args
-	end
+    def columns write_headers: true, &block
+        @export_columns = ExportColumns.new self
+        @export_columns.instance_eval &block
+        @write_headers = write_headers
+    end
 
-	def self.target_path(path) = @@target_path = path
-	def self.instances = @@configs
+    def run
+        @issues = Extractor.new(@file_prefix, @@target_path).run
+        self.instance_eval &@export_config_block
 
-	def initialize file_prefix:, project: nil, filter: nil, jql: nil, export_config_block: nil
-		@file_prefix = file_prefix
-		@csv_filename = "#{@@target_path}/#{file_prefix}.csv"
-		@export_config_block = export_config_block
-		@jql = make_jql project: project, filter: filter, jql: jql
-		@project_key = project
-	end
+        File.open(@csv_filename, 'w') do |file|
+            if @write_headers
+                line = @export_columns.columns.collect { |type, label, proc| label }
+                file.puts CSV.generate_line(line)
+            end
+            @issues.each do |issue|
+                line = []
+                @export_columns.columns.each do |type, name, block|
+                    # Invoke the block that will retrieve the result from Issue
+                    result = instance_exec(issue, &block)
+                    # Convert that result to the appropriate type
+                    line << __send__(:"to_#{type}", result)
+                end
+                file.puts CSV.generate_line(line)
+            end
+        end
+    end
 
-	def make_jql project:, filter:, jql:
-		return jql unless jql.nil?
-		return "project=#{project.inspect}" unless project.nil?
-		return "filter=#{filter.inspect}" unless filter.nil?
-		raise "Everything was nil"
-	end
+    # TODO: to_date needs to know which timezone we're converting to.
+    def to_date object
+        object&.to_date
+    end
 
-	def columns write_headers: true, &block
-		@export_columns = ExportColumns.new
-		@export_columns.instance_eval &block
-		@write_headers = write_headers
-	end
-
-	def run
-		@issues = Extractor.new(@file_prefix, @@target_path).run
-		self.instance_eval &@export_config_block
-
-		File.open(@csv_filename, 'w') do |file|
-			if @write_headers
-				line = @export_columns.columns.collect { |type, label, proc| label }
-				file.puts CSV.generate_line(line)
-			end
-			@issues.each do |issue|
-				line = []
-				@export_columns.columns.each do |type, name, block|
-					# Invoke the block that will retrieve the result from Issue
-					result = instance_exec(issue, &block)
-					# Convert that result to the appropriate type
-					line << __send__(:"to_#{type}", result)
-				end
-				file.puts CSV.generate_line(line)
-			end
-		end
-	end
-
-	# TODO: to_date needs to know which timezone we're converting to.
-	def to_date object
-		object&.to_date
-	end
-
-	def to_string object
-		object.to_s
-	end
+    def to_string object
+        object.to_s
+    end
 
 end
--- lib/issue.rb
+++ lib/issue.rb
@@ -1,62 +1,67 @@
 class Issue
-	attr_reader :changes
+    attr_reader :changes
 
-	def initialize raw_data
-		@raw = raw_data
-		@changes = []
-		if @raw['changelog'].nil?
-			raise "No changelog found in issue #{@raw['key']}"
-		else
-			@raw['changelog']['histories'].each do |history|
-				created = history['created']
-				history['items'].each do |item|
-					@changes << ChangeItem.new(raw: item, time: created)
-				end
-			end
-		end
+    def initialize raw_data
+        @raw = raw_data
+        @changes = []
+        if @raw['changelog'].nil?
+            raise "No changelog found in issue #{@raw['key']}"
+        else
+            @raw['changelog']['histories'].each do |history|
+                created = history['created']
+                history['items'].each do |item|
+                    @changes << ChangeItem.new(raw: item, time: created)
+                end
+            end
+        end
 
-		# Initial creation isn't considered a change so Jira doesn't create an entry for that
-		@changes << createFakeChangeForCreation
+        # Initial creation isn't considered a change so Jira doesn't create an entry for that
+        @changes << createFakeChangeForCreation
 
-		@changes.reverse!
-	end
+        @changes.reverse!
+    end
 
-	def key = @raw['key']
-	def type = @raw['fields']['issuetype']['name']
-	def summary = @raw['fields']['summary']
+    def key = @raw['key']
+    def type = @raw['fields']['issuetype']['name']
+    def summary = @raw['fields']['summary']
 
-	def createFakeChangeForCreation
-		created_time = @raw['fields']['created']
-		first_status = '--CREATED--'
-		unless @changes.empty?
-			first_status = @changes[-1].raw['fromString'] || first_status
-		end
-		ChangeItem.new time: created_time, field: 'status', value: first_status
-	end
+    def createFakeChangeForCreation
+        created_time = @raw['fields']['created']
+        first_status = '--CREATED--'
+        unless @changes.empty?
+            first_status = @changes[-1].raw['fromString'] || first_status
+        end
+        ChangeItem.new time: created_time, field: 'status', value: first_status
+    end
 
-	def first_time_in_status *status_names
-		@changes.find { |change| change.field == 'status' && status_names.include?(change.value) }&.time
-	end
+    def first_time_in_status *status_names
+        @changes.find { |change| change.field == 'status' && status_names.include?(change.value) }&.time
+    end
 
-	def first_time_not_in_status *status_names
-		@changes.find { |change| change.field == 'status' && status_names.include?(change.value) == false }&.time
-	end
+    def first_time_not_in_status *status_names
+        @changes.find { |change| change.field == 'status' && status_names.include?(change.value) == false }&.time
+    end
 
-	def last_time_in_status *status_names
-		@changes.reverse.find { |change| change.field == 'status' && status_names.include?(change.value) }&.time
-	end
+    def last_time_in_status *status_names
+        @changes.reverse.find { |change| change.field == 'status' && status_names.include?(change.value) }&.time
+    end
 
-	def first_time_for_any_status_change
-		if @changes.size >= 2
-			@changes[1].time
-		else
-			nil
-		end
-	end
-	# last_time_not_in_status(...)
-	# first_time_in_status_category(...)
-	# last_time_in_status_category(...)
-	# first_time_on_board (looking at the board config)
+    def first_time_for_any_status_change
+        if @changes.size >= 2
+            @changes[1].time
+        else
+            nil
+        end
+    end
+
+    def first_time_in_status_category config, *category_names
+        # puts "config=#{config.class} category_names=#{category_names}"
+    end
+
+    # last_time_not_in_status(...)
+    # first_time_in_status_category(...)
+    # last_time_in_status_category(...)
+    # first_time_on_board (looking at the board config)
 
 
 end
