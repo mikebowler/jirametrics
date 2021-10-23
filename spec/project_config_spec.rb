b9136544959b5c3431bc4f3ede5c5658b6336939
Mike Bowler <mbowler@gargoylesoftware.com>
2021-10-23 11:30:18 -0700

    more tests

--- spec/project_config_spec.rb
+++ spec/project_config_spec.rb
@@ -18,4 +18,25 @@ describe ProjectConfig do
       expect(config.category_for(type: 'Story', status: 'Doing', issue_id: 'SP-1')).to eql 'InProgress'
     end
   end
+
+  it 'board_configuration' do
+    config = ProjectConfig.new exporter: nil, target_path: 'spec/testdata/', jira_config: nil, block: nil
+    config.file_prefix 'sample'
+    config.load_board_configuration
+    expect(config.all_board_columns.keys).to eq ['1']
+
+    contents = config.all_board_columns['1'].collect do |column|
+      [column.name, column.status_ids, column.min, column.max]
+    end
+
+    # rubocop:disable Layout/ExtraSpacing
+    expect(contents).to eq [
+      ['Backlog',     [10_000], nil, nil],
+      ['Ready',       [10_001],   1,   4],
+      ['In Progress',      [3], nil,   3],
+      ['Review',      [10_011], nil,   3],
+      ['Done',        [10_002], nil, nil]
+    ]
+    # rubocop:enable Layout/ExtraSpacing
+  end
 end
