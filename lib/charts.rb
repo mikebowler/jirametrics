a3bacfac0ea34be24a9f9ec7602e4544f2f9f3f8
Mike Bowler <mbowler@gargoylesoftware.com>
2021-12-04 08:06:48 -0800

    Upgraded to newest chartjs and got tooltips working

--- lib/charts.rb
+++ lib/charts.rb
@@ -12,30 +12,20 @@ class HTMLReport
     File.open('chart.html', 'w') do |file|
       @file = file
       @file.puts '<html><head>'
-      @file.puts '<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>'
-      # @file.puts '<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js" integrity="sha512-qTXRIMyZIFb8iQcfjXWCO8+M5Tbc38Qi5WzdPOYZHIlZpzBHG3L3by84BBBOiRGiEb7KKtAOAs5qYdUiZiQNNQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>'
-      # @file.puts '<script src="https://cdnjs.cloudflare.com/ajax/libs/date-fns/1.30.1/date_fns.min.js" integrity="sha512-F+u8eWHrfY8Xw9BLzZ8rG/0wIvs0y+JyRJrXjp3VjtFPylAEEGwKbua5Ip/oiVhaTDaDs4eU2Xtsxjs/9ag2bQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>'
-      # @file.puts '<script src="https://cdn.jsdelivr.net/npm/chart.js/dist/chart.min.js"></script>'
 
-    # @file.puts '<script src="http://cdnjs.cloudflare.com/ajax/libs/moment.js/2.13.0/moment.min.js"></script>'
-    @file.puts '<script src="https://cdnjs.cloudflare.com/ajax/libs/chartjs-adapter-moment/1.0.0/chartjs-adapter-moment.js" integrity="sha512-ADAbyMMmwwyHVtg8yjnVPK2p4YyMiD3Hh05WPBm3F/F/u01dPpGPfqJaVSLHVxSXv0u+h6WRxyxDbihxLws9ig==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>'
-    @file.puts '<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>'
-    @file.puts '<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.bundle.js"></script>'
-    # @file.puts '<script src="https://cdn.jsdelivr.net/npm/chart.js@3.6.1/dist/chart.min.js"></script>'
+      @file.puts '<script src="https://cdn.jsdelivr.net/npm/moment@2.29.1/moment.js"></script>'
+      @file.puts '<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>'
+      @file.puts '<script src="https://cdn.jsdelivr.net/npm/chartjs-adapter-moment@^1"></script>'
 
       @file.puts '</head><body>'
       @file.puts '<div>Hello world</div>'
-      scatterplot
+      cycletime_scatterplot
       @file.puts '</html>'
     end
   end
 
-  def scatterplot
+  def cycletime_scatterplot
     completed_issues = @all_issues.select { |issue| issue.cycle_time }
-
-    # data = xcompleted_issues.collect do |issue|
-    #   { 'y' => issue.cycle_time, 'x' => issue.column_times[-1].to_date }
-    # end
 
     colours = %w[blue green orange yellow gray black]
     data_sets = []
@@ -44,7 +34,12 @@ class HTMLReport
         'label' => type,
         'data' => completed_issues
           .select { |issue| issue.type == type }
-          .collect { |issue| { 'y' => issue.cycle_time, 'x' => issue.column_times[-1].to_date, 'title' => 'foo' } },
+          .collect do |issue|
+            { 'y' => issue.cycle_time,
+              'x' => issue.column_times[-1].to_date,
+              'title' => ["#{issue.key} : #{issue.cycle_time} day#{'s' unless issue.cycle_time == 1}",issue.title]
+            }
+          end,
         'fill' => false,
         'showLine' => false,
         'backgroundColor' => colours[index]
@@ -69,7 +64,7 @@ class HTMLReport
         },
         responsive: false, // If responsive is true then it fills the screen
         scales: {
-          xAxes: [{
+          x: {
             type: "time",
             time: {
                 format: 'YYYY-MM-DD'
@@ -78,19 +73,19 @@ class HTMLReport
               display: true,
               labelString: 'Date Completed'
             }
-          }],
-          yAxes: [{
+          },
+          y: {
             scaleLabel: {
               display: true,
               labelString: 'Days'
             }
-          }]
+          }
         },
         plugins: {
           tooltip: {
             callbacks: {
               label: function(context) {
-                return 'foo';
+                return context.dataset.data[context.dataIndex].title
               }
             }
           }
@@ -104,10 +99,8 @@ class HTMLReport
   end
 end
 
-
-
 # ======================== #
-#ID,link,title,Backlog,ANALYSIS,DEFINED,DEV & PEER REVIEW,PREPARING FOR TEST,IN TEST,DONE,type,sprint_count,% blocked
+# TODO: It's currently hardcoded to know how many columns there will be and what they'll be.
 class Issue
   attr_reader :key, :title, :column_times, :type, :cycle_time
 
@@ -118,9 +111,9 @@ class Issue
     @type = row[10].inspect
     @cycle_time = (@column_times[-1] - @column_times[0]).to_i unless @column_times[-1].nil?
 
-    puts '', row.inspect
-    puts @column_times.inspect
-    puts @cycle_time
+    # puts '', row.inspect
+    # puts @column_times.inspect
+    # puts @cycle_time
   end
 end
 
