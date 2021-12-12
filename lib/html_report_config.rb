0cb781f6148cf169faf037b8487c3487bb710d09
Mike Bowler <mbowler@gargoylesoftware.com>
2021-12-11 19:00:33 -0800

    Still working on the 'look' for charts

--- lib/html_report_config.rb
+++ lib/html_report_config.rb
@@ -67,6 +67,128 @@ class HtmlReportConfig
     @sections << render(binding)
   end
 
+  def total_wip_over_time_chart
+    list = []
+    active_issues = []
+    @file_config.issues.each do |issue|
+      started = @cycletime.started_time(issue)
+      stopped = @cycletime.stopped_time(issue)
+      next unless started
+
+      list << [started, 'start', issue]
+      list << [@cycletime.stopped_time(issue), 'stop', issue] unless stopped.nil?
+    end
+
+    chart_data = []
+
+    days_issues = []
+    days_issues_completed = []
+    list.sort! { |a, b| a.first <=> b.first }
+    current_date = list.first.first.to_date
+    list.each do |time, action, issue|
+      new_date = time.to_date
+      unless new_date == current_date
+        chart_data << [current_date, days_issues.uniq, days_issues_completed]
+        days_issues = []
+        days_issues_completed = []
+        current_date = new_date
+      end
+
+      days_issues << issue
+      if action == 'start'
+        active_issues << issue
+      elsif action == 'stop'
+        active_issues.delete(issue)
+        days_issues_completed << issue
+      else
+        raise "Unexpected action #{action}"
+      end
+    end
+
+    today = Date.today
+    graphable_date_range = (today - 90..today)
+    data_sets = []
+    data_sets << {
+      'type' => 'bar',
+      'label' => 'Completed',
+      'data' => chart_data.collect do |time, _issues, issues_completed|
+        if time >= graphable_date_range.begin && time < graphable_date_range.end
+          {
+            x: time,
+            y: -issues_completed.size,
+            title: ['Work items completed'] + issues_completed.collect { |i| "#{i.key} : #{i.summary}" }
+          }
+        else
+          nil
+        end
+      end.compact,
+      backgroundColor: 'green'
+    }
+    data_sets << {
+      'type' => 'bar',
+      'label' => 'More than four weeks',
+      'data' => foo_data_set(chart_data: chart_data, min_age: 29, max_age: nil, label: 'More than four weeks'),
+      'backgroundColor' => 'red'
+    }
+    data_sets << {
+      'type' => 'bar',
+      'label' => 'Less than four weeks',
+      'data' => foo_data_set(chart_data: chart_data, min_age: 14, max_age: 28, label: 'Less than four weeks'),
+      'backgroundColor' => 'purple'
+    }
+    data_sets << {
+      'type' => 'bar',
+      'label' => 'Less than two weeks',
+      'data' => foo_data_set(chart_data: chart_data, min_age: 0, max_age: 3, label: 'Less than two weeks'),
+      'backgroundColor' => 'brown'
+    }
+    data_sets << {
+      'type' => 'bar',
+      'label' => 'Less than a week',
+      'data' => foo_data_set(chart_data: chart_data, min_age: 3, max_age: 8, label: 'Less than a week'),
+      'backgroundColor' => 'gray'
+    }
+    data_sets << {
+      'type' => 'bar',
+      'label' => 'Less than two days',
+      'data' => foo_data_set(chart_data: chart_data, min_age: 0, max_age: 3, label: 'Less than two days'),
+      'backgroundColor' => 'lightgray'
+    }
+    @sections << render(binding)
+  end
+
+  def foo_data_set chart_data:, min_age:, max_age:, label:
+    # chart_data is a list of [time, issues, issues_completed] groupings
+
+    today = Date.today
+    graphable_date_range = (today - 90..today)
+    chart_start_date = graphable_date_range.begin
+    chart_end_date = graphable_date_range.end
+    # chart_end_date = Date.today
+    # chart_start_date = chart_end_date - 90 #chart_data.first.first.to_date
+    data = [chart_start_date, [], []]
+    chart_start_date.upto(chart_end_date).collect do |date|
+      # Not all days have data. For days that don't, use the previous days data
+      data = chart_data.find { |a| a.first == date } || data
+      time, issues, _issues_completed = *data
+
+      included_issues = issues.collect do |issue|
+        age = (time - @cycletime.started_time(issue)).to_i + 1
+        if age >= min_age && (max_age.nil? || age < max_age)
+          [issue, age]
+        else
+          nil
+        end
+      end.compact
+
+      {
+        x: date,
+        y: included_issues.size,
+        title: [label] + included_issues.collect { |i,age| "#{i.key} : #{i.summary} (#{age})" }
+      } 
+    end
+  end
+
   def cycletime_scatterplot
     completed_issues = @file_config.issues.select { |issue| @cycletime.done? issue }
     data_sets = []
