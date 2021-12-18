1133daf1567a9a069e4dd90fd8f31a35adbb9880
Mike Bowler <mbowler@gargoylesoftware.com>
2021-12-18 08:38:35 -0800

    Refactored a bunch of setup code into a let block

--- spec/aging_work_in_progress_chart_spec.rb
+++ spec/aging_work_in_progress_chart_spec.rb
@@ -13,11 +13,16 @@ def complete_sample_board_metadata
 end
 
 describe AgingWorkInProgressChart do
+  let :chart do
+    chart = AgingWorkInProgressChart.new
+    chart.board_metadata = complete_sample_board_metadata
+    chart.cycletime = defaultCycletimeConfig
+    chart.issues = complete_sample_issues
+    chart
+  end
+
   context 'accumulated_status_ids_per_column' do
     it 'should accumulate properly' do
-      chart = AgingWorkInProgressChart.new
-      chart.board_metadata = complete_sample_board_metadata
-      chart.cycletime = defaultCycletimeConfig
       chart.issues = complete_sample_issues.select { |issue| chart.cycletime.in_progress? issue }
 
       actual = chart.accumulated_status_ids_per_column
@@ -33,9 +38,6 @@ describe AgingWorkInProgressChart do
 
   context 'ages_of_issues_that_crossed_column_boundary' do
     it 'should handle no issues' do
-      chart = AgingWorkInProgressChart.new
-      chart.board_metadata = complete_sample_board_metadata
-      chart.cycletime = defaultCycletimeConfig
       issues = []
 
       actual = chart.ages_of_issues_that_crossed_column_boundary issues: issues, status_ids: [10_002, 10_011]
@@ -43,66 +45,36 @@ describe AgingWorkInProgressChart do
     end
 
     it 'should handle no status ids' do
-      chart = AgingWorkInProgressChart.new
-      chart.board_metadata = complete_sample_board_metadata
-      chart.cycletime = defaultCycletimeConfig
-      issues = complete_sample_issues
-
-      actual = chart.ages_of_issues_that_crossed_column_boundary issues: issues, status_ids: []
+      actual = chart.ages_of_issues_that_crossed_column_boundary issues: chart.issues, status_ids: []
       expect(actual).to eq []
     end
 
     it 'should handle happy path' do
-      chart = AgingWorkInProgressChart.new
-      chart.board_metadata = complete_sample_board_metadata
-      chart.cycletime = defaultCycletimeConfig
-      issues = complete_sample_issues
-
-      actual = chart.ages_of_issues_that_crossed_column_boundary issues: issues, status_ids: [10_002, 10_011, 3]
+      actual = chart.ages_of_issues_that_crossed_column_boundary issues: chart.issues, status_ids: [10_002, 10_011, 3]
       expect(actual).to eq [179, 72, 1]
     end
   end
 
   context 'days_at_percentage_threshold_for_all_columns' do
     it 'at 100%' do
-      chart = AgingWorkInProgressChart.new
-      chart.board_metadata = complete_sample_board_metadata
-      chart.cycletime = defaultCycletimeConfig
-      issues = complete_sample_issues
-
-      actual = chart.days_at_percentage_threshold_for_all_columns percentage: 100, issues: issues
+      actual = chart.days_at_percentage_threshold_for_all_columns percentage: 100, issues: chart.issues
       expect(actual).to eq [0, 0, 0, 0, 0]
     end
 
     it 'at 0%' do
-      chart = AgingWorkInProgressChart.new
-      chart.board_metadata = complete_sample_board_metadata
-      chart.cycletime = defaultCycletimeConfig
-      issues = complete_sample_issues
-
-      actual = chart.days_at_percentage_threshold_for_all_columns percentage: 0, issues: issues
+      actual = chart.days_at_percentage_threshold_for_all_columns percentage: 0, issues: chart.issues
       expect(actual).to eq [1, 1, 1, 80, 80]
     end
   end
 
   context 'column_for' do
     it 'should work' do
-      chart = AgingWorkInProgressChart.new
-      chart.board_metadata = complete_sample_board_metadata
-      chart.cycletime = defaultCycletimeConfig
-      issues = complete_sample_issues
-
-      actual = chart.column_for(issue: issues[-1]).name
+      actual = chart.column_for(issue: chart.issues[-1]).name
       expect(actual).to eq 'Review'
     end
   end
 
   it 'make_data_sets' do
-    chart = AgingWorkInProgressChart.new
-    chart.board_metadata = complete_sample_board_metadata
-    chart.cycletime = defaultCycletimeConfig
-    chart.issues = complete_sample_issues
-
     expect(chart.make_data_sets).to eq([
       {
         'backgroundColor' => 'green',
