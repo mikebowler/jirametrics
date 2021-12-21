fcddefbf184b0a10ff7933777e8e4952749aebf9
Mike Bowler <mbowler@gargoylesoftware.com>
2021-12-21 12:01:00 -0800

    Generate chart ids so that we can have multiple of the same chart on the same page

--- lib/chart_base.rb
+++ lib/chart_base.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
 class ChartBase
+  @@chart_counter = 0
+
   def render caller_binding, file
     basename = Pathname.new(File.realpath(file)).basename.to_s
     raise "Unexpected filename #{basename.inspect}" unless basename =~ /^(.+)\.rb$/
+
+    # Insert a 'random' chart_id so that all the chart names on the page are unique
+    caller_binding.eval "chart_id='chart#{@@chart_counter += 1}'"
 
     erb = ERB.new File.read "html/#{$1}.erb"
     erb.result(caller_binding)
