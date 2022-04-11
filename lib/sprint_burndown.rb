d5ab203e86ad3723cc1d6dfe7fd5fb2b2be2e498
Mike Bowler <mbowler@gargoylesoftware.com>
2022-04-11 15:22:27 -0700

    Changed yellow to brown for better readability

--- lib/sprint_burndown.rb
+++ lib/sprint_burndown.rb
@@ -27,7 +27,7 @@ class SprintBurndown < ChartBase
 
     data_sets = []
     sprints.each_with_index do |sprint, index|
-      color = %w[blue orange green red yellow][index % 5]
+      color = %w[blue orange green red brown][index % 5]
       label = sprint.name
       data_sets << {
         label: label,
