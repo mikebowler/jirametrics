14904b0d3fda6d5332760dc2350c35ae12d415da
Mike Bowler <mbowler@gargoylesoftware.com>
2022-04-14 17:39:05 -0700

    Fixed exception when file not found

--- experimental/info.rb
+++ experimental/info.rb
@@ -11,8 +11,10 @@ class InfoDumper
   def run key
     find_file_prefixes.each do |prefix|
       path = "#{@target_dir}#{prefix}_issues/#{key}.json"
-      issue = Issue.new raw: JSON.parse(File.read(path))
-      dump issue
+      if File.exist?(path)
+        issue = Issue.new raw: JSON.parse(File.read(path))
+        dump issue
+      end
     end
   end
 
