77e70612aa813f1e9aed8be49a66acc28acd40a0
Mike Bowler <mbowler@gargoylesoftware.com>
2022-08-09 18:17:01 -0700

    Allow multiple statuses instead of just one

--- lib/discard_changes_before.rb
+++ lib/discard_changes_before.rb
@@ -3,10 +3,11 @@
 module DiscardChangesBefore
   def discard_changes_before status_becomes: nil, &block
     if status_becomes
+      status_becomes = [status_becomes] unless status_becomes.is_a? Array
       block = lambda do |issue|
         time = nil
         issue.changes.each do |change|
-          time = change.time if change.status? && change.value == status_becomes && change.artificial? == false
+          time = change.time if change.status? && status_becomes.include?(change.value) && change.artificial? == false
         end
         time
       end
