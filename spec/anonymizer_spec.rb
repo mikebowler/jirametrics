2d46e240633b4c1675e5bc446ffb46ad7f50b075
Mike Bowler <mbowler@gargoylesoftware.com>
2022-08-14 14:40:10 -0700

    Initial test for the anonymizer. Needs to be much better but this is a start

--- spec/anonymizer_spec.rb
+++ spec/anonymizer_spec.rb
@@ -4,13 +4,18 @@ require './spec/spec_helper'
 
 describe Anonymizer do
 
+  # This is done as an end-to-end test because everything in the anonymizer is interconnected and I was
+  # too lazy to figure a clean way of slicing the parts out.
+
   exporter = Exporter.new
-  project_config = ProjectConfig.new exporter: exporter, target_path: 'spec/complete_sample/', jira_config: nil, block: nil
+  project_config = ProjectConfig.new(
+    exporter: exporter, target_path: 'spec/complete_sample/', jira_config: nil, block: nil
+  )
   project_config.file_prefix 'sample'
   project_config.anonymize
   project_config.run
 
-  it 'should not blow up' do
-    expect(project_config.issues.size).to eq 6
+  it 'should have renumbered all issue keys' do
+    expect(project_config.issues.collect(&:key).sort).to eq %w[ANON-2 ANON-3 ANON-4 ANON-5 ANON-6 ANON-7]
   end
 end
