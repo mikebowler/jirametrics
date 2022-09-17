a77e0c5db18f29e1ef248f11b5a5998122b82990
Mike Bowler <mbowler@gargoylesoftware.com>
2022-09-17 11:06:58 -0700

    If the slope is steep then the trendline can force the diagram to have a massive y axis, which is undesirable. Fixed the trend line so it won't draw above the max cycletime

--- lib/trend_line_calculator.rb
+++ lib/trend_line_calculator.rb
@@ -17,46 +17,58 @@ class TrendLineCalculator
     @slope = ((n * sum_of_xy) - (sum_of_x * sum_of_y)) / ((n * sum_of_x2) - (sum_of_x * sum_of_x))
     @offset = (sum_of_y - (@slope * sum_of_x)) / n
 
-    # If the line is vertical then consider it invalid.
-    @valid = false if @slope.infinite?
+    @valid = false if vertical?
   end
 
   def valid? = @valid
   def horizontal? = @slope.zero?
+  def vertical? = @slope.nan?
 
   def calc_y x: # rubocop:disable Naming/MethodParameterName
-    (x * @slope) + @offset
+    ((x * @slope) + @offset).to_i
   end
 
-  def calc_x_where_y_is_zero
-    raise 'y will never be zero. Trend is perfectly horizontal' if horizontal?
+  def line_crosses_at y: # rubocop:disable Naming/MethodParameterName
+    raise "line will never cross #{y}. Trend is perfectly horizontal" if horizontal?
 
-    -(@offset / @slope)
+    ((y.to_f - @offset) / @slope).to_i
   end
 
   # If the trend line can't be calculated then return an empty array. Otherwise, return
   # an array with two (x,y) points, with which you can draw the trend line.
-  def chart_datapoints range:
+  def chart_datapoints range:, max_y:, min_y: 0
+    raise 'max_y is nil' if max_y.nil?
+    return [] unless valid?
+
     data_points = []
-    if valid?
-      x_start = range.begin
-      y_start = calc_y(x: range.begin).to_i
-      x_end = range.end
-      y_end = calc_y(x: range.end).to_i
+    x_start = range.begin
+    y_start = calc_y(x: range.begin).to_i
+    x_end = range.end
+    y_end = calc_y(x: range.end).to_i
 
-      if y_start.negative?
-        x_start = calc_x_where_y_is_zero.to_i
-        y_start = 0
-      end
-
-      if y_end.negative?
-        x_end = calc_x_where_y_is_zero.to_i
-        y_end = 0
-      end
-
-      data_points << { x: x_start, y: y_start }
-      data_points << { x: x_end, y: y_end }
+    if y_start < min_y
+      x_start = line_crosses_at y: 0
+      y_start = 0
     end
+
+    if y_start > max_y
+      x_start = line_crosses_at y: max_y
+      y_start = max_y
+    end
+
+    if y_end < min_y
+      x_end = line_crosses_at y: 0
+      y_end = 0
+    end
+
+    if y_end > max_y
+      x_end = line_crosses_at y: max_y
+      y_end = max_y
+    end
+
+    data_points << { x: x_start, y: y_start }
+    data_points << { x: x_end, y: y_end }
+
     data_points
   end
 end
--- spec/trend_line_calculator_spec.rb
+++ spec/trend_line_calculator_spec.rb
@@ -31,29 +31,31 @@ describe TrendLineCalculator do
     end
   end
 
-  context 'calc_x_where_y_is_zero' do
+  context 'line_crosses_at' do
     it 'calculates for two simple points' do
       calculator = TrendLineCalculator.new [[4, 3], [3, 2]]
       expect(calculator.horizontal?).to be_falsey
-      expect(calculator.calc_x_where_y_is_zero).to eq 1.0
+      expect(calculator.line_crosses_at y: 0).to eq 1.0
     end
 
     it 'calculates for a perfect horizontal trend' do
       calculator = TrendLineCalculator.new [[1, 2], [2, 2]]
       expect(calculator.horizontal?).to be_truthy
-      expect { calculator.calc_x_where_y_is_zero }.to raise_error('y will never be zero. Trend is perfectly horizontal')
+      expect { calculator.line_crosses_at y: 0 }.to raise_error(
+        'line will never cross 0. Trend is perfectly horizontal'
+      )
     end
   end
 
   context 'chart_datapoints' do
     it 'should return empty array when trend cant be calculated' do
       calculator = TrendLineCalculator.new []
-      expect(calculator.chart_datapoints range: 3..4).to be_empty
+      expect(calculator.chart_datapoints range: 3..4, max_y: 100).to be_empty
     end
 
     it 'should return a pair of points for a perfect horizontal' do
       calculator = TrendLineCalculator.new [[1, 2], [2, 2]]
-      expect(calculator.chart_datapoints range: 3..4).to eq([
+      expect(calculator.chart_datapoints range: 3..4, max_y: 100).to eq([
         { x: 3, y: 2 },
         { x: 4, y: 2 }
       ])
@@ -61,7 +63,7 @@ describe TrendLineCalculator do
 
     it 'should stop at zero for a descending line' do
       calculator = TrendLineCalculator.new [[1, 2], [2, 1]]
-      expect(calculator.chart_datapoints range: 1..4).to eq([
+      expect(calculator.chart_datapoints range: 1..4, max_y: 100).to eq([
         { x: 1, y: 2 },
         { x: 3, y: 0 }
       ])
@@ -69,10 +71,36 @@ describe TrendLineCalculator do
 
     it 'should start at zero for a ascending line' do
       calculator = TrendLineCalculator.new [[12, 3], [13, 4]]
-      expect(calculator.chart_datapoints range: 0..15).to eq([
+      expect(calculator.chart_datapoints range: 0..15, max_y: 100).to eq([
         { x: 9, y: 0 },
         { x: 15, y: 6 }
       ])
+    end
+
+    it 'should not exceed max_y for an ascending line' do
+      calculator = TrendLineCalculator.new [[12, 3], [13, 4]]
+      expect(calculator.chart_datapoints range: 0..15, max_y: 5).to eq([
+        { x: 9, y: 0 },
+        { x: 14, y: 5 }
+      ])
+    end
+
+    it 'should not exceed max_y for a descending line' do
+      calculator = TrendLineCalculator.new [[12, 9], [13, 8]]
+      expect(calculator.chart_datapoints range: 0..15, max_y: 10).to eq([
+        { x: 11, y: 10 },
+        { x: 15, y: 6 }
+      ])
+    end
+
+    it 'should handle a vertical line' do
+      calculator = TrendLineCalculator.new [[12, 9], [12, 8]]
+      expect(calculator.chart_datapoints range: 0..15, min_y: 2, max_y: 10).to eq([])
+    end
+
+    it 'should raise error if max_y is nil' do
+      calculator = TrendLineCalculator.new [[12, 9], [12, 8]]
+      expect { calculator.chart_datapoints range: 0..15, max_y: nil }.to raise_error 'max_y is nil'
     end
   end
 end
