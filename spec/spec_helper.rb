9a20b1837bdc31eafe0ca49921d678ca0d424dc0
Mike Bowler <mbowler@gargoylesoftware.com>
2022-11-09 16:35:03 -0800

    fixed typo

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -118,7 +118,7 @@ end
 def to_time string
   if string =~ /^(\d{4})-(\d{2})-(\d{2})$/
     Time.new $1.to_i, $2.to_i, $3.to_i, 0, 0, 0, '+00:00'
-  if string =~ /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})$/
+  elsif string =~ /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})$/
     Time.new $1.to_i, $2.to_i, $3.to_i, $4.to_i, $5.to_i, $6.to_i, '+00:00'
   else
     Time.parse string
