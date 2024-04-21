fc274a12000eaf6fdfe467f08411224294d14126
Mike Bowler <mbowler@gargoylesoftware.com>
2024-04-21 09:45:06 -0700

    Removed more deprecated stuff

--- lib/jirametrics/download_config.rb
+++ lib/jirametrics/download_config.rb
@@ -15,25 +15,6 @@ class DownloadConfig
     instance_eval(&@block)
   end
 
-  def project_key _key = nil
-    raise 'project, filter, and jql directives are no longer supported. See ' \
-      'https://github.com/mikebowler/jira-export/wiki/Deprecated#project-filter-and-jql-are-no-longer-supported-in-the-download-section'
-  end
-
-  def board_ids *ids
-    deprecated message: 'board_ids in the download block are deprecated. See https://github.com/mikebowler/jira-export/wiki/Deprecated'
-    @board_ids = ids unless ids.empty?
-    @board_ids
-  end
-
-  def filter_name _filter = nil
-    project_key
-  end
-
-  def jql _query = nil
-    project_key
-  end
-
   def rolling_date_count count = nil
     @rolling_date_count = count unless count.nil?
     @rolling_date_count
