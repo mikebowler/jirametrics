f477d7b4c1877172ebbe7cb9d8187c12ea9888d9
Mike Bowler <mbowler@gargoylesoftware.com>
2026-01-06 09:12:28 -0800

    Added methods to fix_version to access other data that's present

--- lib/jirametrics/fix_version.rb
+++ lib/jirametrics/fix_version.rb
@@ -11,11 +11,24 @@ class FixVersion
     @raw['name']
   end
 
+  def description
+    @raw['description']
+  end
+
   def id
     @raw['id'].to_i
   end
 
+  def release_date
+    text = @raw['releaseDate']
+    text.nil? ? nil : Date.parse(text)
+  end
+
   def released?
     @raw['released']
+  end
+
+  def archived?
+    @raw['archived']
   end
 end
--- spec/fix_version_spec.rb
+++ spec/fix_version_spec.rb
@@ -3,10 +3,24 @@
 require './spec/spec_helper'
 
 describe FixVersion do
-  let(:fix_version) { described_class.new({ 'name' => 'Barney', 'id' => '2', 'released' => true }) }
+  let(:fix_version) do
+    described_class.new({
+      'self' => 'https://improvingflow.atlassian.com/rest/api/2/version/50827',
+      'id' => '2',
+      'description' => 'my description',
+      'name' => 'my name',
+      'archived' => true,
+      'released' => true,
+      'releaseDate' => '2022-12-25'
+      })
+  end
 
   it 'knows its name' do
-    expect(fix_version.name).to eq 'Barney'
+    expect(fix_version.name).to eq 'my name'
+  end
+
+  it 'knows its description' do
+    expect(fix_version.description).to eq 'my description'
   end
 
   it 'knows its id' do
@@ -15,5 +29,18 @@ describe FixVersion do
 
   it 'knows if it is released' do
     expect(fix_version).to be_released
+  end
+
+  it 'knows if it is archived' do
+    expect(fix_version).to be_archived
+  end
+
+  it 'knows release date' do
+    expect(fix_version.release_date).to eq Date.parse('2022-12-25')
+  end
+
+  it 'succeeds when release date missing' do
+    fix_version.raw['releaseDate'] = nil
+    expect(fix_version.release_date).to be_nil
   end
 end
