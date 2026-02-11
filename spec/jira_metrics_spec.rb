22fccdc5ddc573e9eed78451c00d03b3291529cb
Mike Bowler <mbowler@gargoylesoftware.com>
2026-02-11 08:26:40 -0800

    exit() wasn't properly being handled as as a result, about 150 of the specs weren't running. Fixed

--- spec/jira_metrics_spec.rb
+++ spec/jira_metrics_spec.rb
@@ -1,24 +1,25 @@
 # frozen_string_literal: true
 
+require './spec/spec_helper'
+require './spec/mock_file_system'
+
 # Testing main entrypoint
 describe JiraMetrics do
   let(:jirametrics) { described_class.new }
   let(:mock_file_system) { MockFileSystem.new }
 
   it 'does not find config' do
-    expect do
-      jirametrics.load_config 'missing_config', file_system: mock_file_system
-    end.to raise_error SystemExit
+    jirametrics.load_config 'missing_config', file_system: mock_file_system
+    raise 'It should have exited'
+  rescue SystemExit
     expect(mock_file_system.log_messages).to eq([
       'Error: Cannot find configuration file "missing_config"'
     ])
   end
 
-  it 'loads sucessfully' do
-    mock_file_system.when_loading file: 'my_config.rb', json: 'file_system.log "hello"'
-    jirametrics.load_config 'my_config', file_system: mock_file_system
-    expect(mock_file_system.log_messages).to eq([
-      'hello'
-    ])
+  it 'calls load with the absolute path of the config file' do
+    mock_file_system.when_loading file: 'my_config.rb', json: ''
+    expect(jirametrics).to receive(:load).with(File.absolute_path('my_config.rb')) # rubocop:disable RSpec/MessageSpies
+    jirametrics.load_config 'my_config.rb', file_system: mock_file_system
   end
 end
